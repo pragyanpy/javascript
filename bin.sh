e8ac99691c34866edb46a976c703e52e6b449aab
Pragyan <mail@pragyan.me>
2025-04-11 07:48:02 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,6 +1,1 @@
-#! /bin/bash
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
-
-sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install eyJhIjoiYmZkNWY4NTgwNDRhZjFhNzkxN2RjNDJkNmI4NTFjNWYiLCJ0IjoiZWVkZTkyNDAtMDA1Zi00NTM1LTlmNDctN2QyMzVhNWUxYjBjIiwicyI6IllqTmxZV1ZsTnpBdE9UY3lOeTAwWldaaUxXRTVPRFF0WkRGallqUTRNRGhoTURGayJ9
+null
